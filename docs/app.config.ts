937cb79861e58605022d7e2b55cd7483b40496fa
Ekure Edem <61796351+astraldev@users.noreply.github.com>
2026-02-24 23:14:47 +0100

    docs: improve documentation for grid

---
+++ docs/app.config.ts
@@ -0,0 +1,14 @@
+export default defineAppConfig({
+  header: {
+    title: "Vue Virtual Grid",
+  },
+  socials: {
+    github: "https://github.com/astraldev/vvirtual-grid",
+    npm: "https://www.npmjs.com/package/vvirtual-grid",
+  },
+  github: {
+    url: "https://github.com/astraldev/vvirtual-grid",
+    branch: "main",
+    rootDir: "docs",
+  },
+});
